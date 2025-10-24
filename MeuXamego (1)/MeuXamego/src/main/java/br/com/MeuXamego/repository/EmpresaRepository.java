e6408de9e3db238205eb3830109bc2b2fcc6b54e
Vaktors <147989606+Vaktors@users.noreply.github.com>
2025-10-24 14:40:19 -0300

    Delete MeuXamego (1)/MeuXamego directory

--- MeuXamego (1)/MeuXamego/src/main/java/br/com/MeuXamego/repository/EmpresaRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.MeuXamego.repository;
-
-import br.com.MeuXamego.model.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
-    boolean existsByCnpj(String cnpj);
-}
