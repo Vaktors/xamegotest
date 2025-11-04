9862fb50a1bfbeca45257952aea453d9f7128cdf
Vaktors <147989606+Vaktors@users.noreply.github.com>
2025-11-04 13:37:58 -0300

    Delete src directory

--- src/main/java/br/com/MeuXamego/repository/PessoaFisRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.MeuXamego.repository;
-
-import br.com.MeuXamego.model.PessoaFis;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaFisRepository extends JpaRepository<PessoaFis, Long> {
-    boolean existsByCpf(String cpf);
-}
